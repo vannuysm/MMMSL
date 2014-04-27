dc6062b38c1c346a08e7a15afaac954ca4394279
Matt VanNuys <vannuysm@gmail.com>
2014-04-27 03:25:36 -0400

    Added loading graphic to schedules

--- Mmmsl.Web/app/controllers/divisionScheduleController.js
+++ Mmmsl.Web/app/controllers/divisionScheduleController.js
@@ -10,11 +10,14 @@
         var division = $routeParams['division'];
 
         $scope.title = division + ' Division Games';
+
         $scope.schedule = [];
+        $scope.isLoaded = false;
 
         $http.get($window.sessionStorage.apiUrl + '/api/divisions/' + division + '/schedule')
             .success(function (result) {
                 $scope.schedule = result;
+                $scope.isLoaded = true;
             });
     }
 })();
