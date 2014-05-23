97deb3364d9a3ca40b3f3b840838aca077d0318f
Matt VanNuys <vannuysm@gmail.com>
2014-05-23 01:48:21 -0400

    Added MVC5 back in because troubleshooting assembly not found errors at 2am is not a party

---
+++ Mmmsl.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Mmmsl.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/assets/js/modernizr").Include("~/assets/js/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/assets/js/vendor").Include(
+                "~/assets/js/jquery-{version}.js",
+                "~/assets/js/underscore.js",
+                "~/assets/js/moment.js",
+                "~/assets/js/bootstrap.js",
+                "~/assets/js/bootstrap-datetimepicker.js",
+                "~/assets/js/respond.js",
+                "~/assets/js/retina-*",
+                "~/assets/js/angular.js",
+                "~/assets/js/angular-*",
+                "~/assets/js/ui-bootstrap.js"
+            ));
+
+            bundles.Add(new ScriptBundle("~/app/c").IncludeDirectory("~/app/controllers", "*.js", true));
+            bundles.Add(new ScriptBundle("~/app/a").Include("~/app/app.js"));
+
+            bundles.Add(new StyleBundle("~/assets/css/vendor")
+                .Include("~/assets/css/bootstrap.css")
+                .Include("~/assets/css/bootstrap-datetimepicker.css")
+                .Include("~/assets/css/animate.css")
+                .Include("~/assets/css/font-awesome.css")
+            );
+            bundles.Add(new StyleBundle("~/assets/css/app").Include("~/assets/css/app-*"));
+        }
+    }
+}
---
+++ Mmmsl.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mmmsl.Web.MvcApplication" Language="C#" %>
