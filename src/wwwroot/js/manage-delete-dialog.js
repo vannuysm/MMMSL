0bd92721c9922b8e7e034419a2859404393a7f64
Matt VanNuys <vannuysm@gmail.com>
2017-04-28 15:32:00 -0400

    Enter = delete confirmation, add email tooltip

--- src/wwwroot/js/manage-delete-dialog.js
+++ src/wwwroot/js/manage-delete-dialog.js
@@ -5,6 +5,12 @@
         confirmationTarget = ev.relatedTarget;
     });
 
+    document.getElementById('delete-confirmation').addEventListener('keypress', function (ev) {
+        if (ev.which === 13) {
+            document.getElementById('delete-confirmed-button').click();
+        }
+    });
+
     document.getElementById('delete-confirmed-button').addEventListener('click', function (ev) {
         var form = document.querySelector(confirmationTarget.dataset.targetForm);
 
